6d1b68e90075ad36859412b8dbbe680df1bea778
Sara Bracha <stanenba2@student.touro.edu>
2016-09-13 19:58:36 -0400

    Now it does something!

--- src/MessageDisplayer.java
+++ src/MessageDisplayer.java
@@ -2,6 +2,9 @@
 public class MessageDisplayer {
 	private String msg;
 	public MessageDisplayer(String m){
-		
+		msg = m;
+	}
+	public void display(){
+		System.out.println(msg);
 	}
 }
--- src/Program.java
+++ src/Program.java
@@ -3,5 +3,6 @@ public class Program {
 
 	public static void main(String[] args){
 		MessageDisplayer md = new MessageDisplayer("m");
+		md.display();	//show message
 	}
 }
